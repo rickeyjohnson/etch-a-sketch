320447bab5bb8c70a8f152f71c87c8335d5a1f6c
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-28 10:37:49 -0400

    added button

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-let rows = 16
-let cols = 16
-let containerLength, cellLength
+let sides, containerLength, cellLength, cells
 
 const container = document.querySelector("#container")
 containerLength = container.offsetHeight
-cellLength = containerLength / rows
 
-function constructGrid() {   
-    for (let i = 0; i < (rows * cols); i++) {
+function constructGrid(s) {   
+    for (let i = 0; i < (s ** 2); i++) {
         const cell = document.createElement("div")
         cell.classList.add("cell")
         cell.setAttribute("style", "height: " + cellLength + "px; width: " + cellLength + "px;")
@@ -15,11 +12,16 @@ function constructGrid() {
     }
 }
 
-constructGrid()
+const button = document.querySelector("#user-input")
+button.addEventListener('click', () => {
+    sides = Number.parseInt(prompt("how many squares per side do you want? good sir!"))
+    cellLength = containerLength / sides
+    constructGrid(sides)
 
-const cells = document.querySelectorAll(".cell")
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.classList.add("active")
+    cells = document.querySelectorAll(".cell")
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.classList.add("active")
+        })
     })
 })
