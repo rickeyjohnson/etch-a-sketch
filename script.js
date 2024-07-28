4e461d309bef4ed7596eede6f6a0e24a15ed74c2
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-28 09:26:32 -0400

    added 16 x 16 grid via code in script js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let rows = 16
+let cols = 16
+
 const container = document.querySelector("#container")
+
+// create 16 x 16 grid
+
+for (let i = 0; i < (rows * cols); i++) {
+    const cell = document.createElement("div")
+    cell.classList.add = "cell"
+    cell.setAttribute("style", "width: 10px; height: 10px; background-color: red; border: 2px green solid;")
+    container.appendChild(cell)
+}
