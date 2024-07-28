efa058758e114c5d098d5972ca7a4bceb88534bd
rickeyjohnson <rdjwinter2004@gmail.com>
2024-07-28 09:50:25 -0400

    made the grid

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let rows = 16
 let cols = 16
+let containerLength, cellLength
 
 const container = document.querySelector("#container")
-
-// create 16 x 16 grid
+containerLength = container.offsetHeight
+console.log(containerLength)
+cellLength = containerLength / rows
 
 for (let i = 0; i < (rows * cols); i++) {
     const cell = document.createElement("div")
     cell.classList.add = "cell"
-    cell.setAttribute("style", "width: 10px; height: 10px; background-color: red; border: 2px green solid;")
+    cell.setAttribute("style", "background-color: pink; border: 2px purple solid; height: " + cellLength + "px; width: " + cellLength + "px;")
     container.appendChild(cell)
 }
+
+console.log('javascript ran!')
